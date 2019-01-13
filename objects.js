3cd8c28840c8165c4c06ddb2774452b9c6b708d5
AJ Guiles <ashiguiles@gmail.com>
2019-01-13 03:14:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {artistName:"songTitles"}
 function updatePlaylist(playlist, artistName,songTitles) {
   // playlist['artistName'] =songTitles;
   
-  Object.assign({}, playlist, { artistName: [''] })
+  Object.assign({}, playlist, { artistName: [''Phil Ochs', "Here's to the State of Mississippi'] })
   
   return playlist;
 }
