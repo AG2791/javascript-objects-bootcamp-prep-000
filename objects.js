9a4e9b049b5eb9a9ae432af6b07d7a3e8da20d54
AJ Guiles <ashiguiles@gmail.com>
2019-01-13 03:01:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName:"songTitles"}
 
 function updatePlaylist(playlist, artistName,songTitles) {
-  playlist['artistName'] =songTitles;
+  // playlist['artistName'] =songTitles;
   return playlist;
 }
